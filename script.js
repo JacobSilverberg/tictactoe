4be6b4faf5e722a0f74ec9ef0a37aadd3dca1964
Jacob Silverberg <minkus962@gmail.com>
2023-11-07 17:48:30 -0500

    refactor code, html hooked in, game logic needs fixing

--- script.js
+++ script.js
@@ -3,115 +3,128 @@
 // Player
 // Gameboard
 
-// const gameBoard = (function createGameBoard () {
-//     const gameBoardContainer = document.querySelector("#game-board");
 
-//     for (let i = 0; i < 3; i++) {
-//         const boardRow = document.createElement('div');
-//         boardRow.setAttribute('class', 'game-board-row');
-//         boardRow.setAttribute('id', `game-board-row-${i + 1}`);
-//         for (let j = 0; j < 3; j++) {
-//             const boardTile = document.createElement('div');
-//             boardTile.setAttribute('class', 'board-tile');
-//             boardTile.setAttribute('id', `board-tile-${(i * 3) + (j + 1)}`);
-//             boardTile.textContent = `tile ${(i * 3) + (j + 1)}`;
-//             boardRow.appendChild(boardTile);
-//         }
-//         gameBoardContainer.appendChild(boardRow);
-//     }
-// })();
+let gameController = (function() {
+    let gameActive = true;
+    const winning = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
-// gameBoard;
-
-function gameBoard() {
-    // Create array of tiles to represent gameboard
     const board = [];
     for (let i = 0; i < 9; i++) {
-        board[i] = tile();
-    }
-
-    const getBoard = (i) => board;
-
-    const markTile = (tile, player) => {
-        if (board[tile].getTile() == 0) {
-            board[tile].markTile()
-        } else {
-            return (false);
-        }
-    }
-
-    const printBoard = () => {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                console.log(board[(3 * i) + j].getTile() + "|");
-            }
-            console.log('\n');
-        }
-    }
-
-    return {
-        printBoard
+        board[i] = "";
     };
-}
-
-function tile() {
-    let value = 0;
-
-    const markTile = (player) => {
-        value = player;
-    };
-
-    const getTile = () => value;
-
-    return {
-        markTile,
-        getTile
-    };
-}
-
-
-function gameController (playerOne = "Player One", playerTwo = "Player Two") {
-    const board = gameBoard();
 
     const players = [
         {
-            name: playerOne,
-            token: 1
+          name: "Player One",
+          token: "X"
         },
         {
-            name: playerTwo,
-            token: 2
+          name: "Player Two",
+          token: "O"
         }
     ];
 
     let activePlayer = players[0];
 
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+    function switchPlayers() {
+        if (activePlayer == players[0]) {
+            activePlayer = players[1];
+        }
+        else {
+            activePlayer = players[0];
+        }
+        console.log(`It is ${activePlayer.name}'s turn.`)
     };
 
-    const getActivePlayer = () => activePlayer;
-
-    const printNewRound = () => {
-        board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn.`)
+    function makeMove(tileSelection) {
+        if (board[tileSelection] == "") {
+            board[tileSelection] = activePlayer.token;
+            document.getElementById(tileSelection).textContent = activePlayer.token;
+            return (true);
+        }
+        else {
+            return (false);
+        }
     }
 
-    const playRound = (tile) => {
-        console.log(`Placing ${getActivePlayer().name}'s piece into tile ${tile}.`)
-        board.markTile(tile, getActivePlayer());
+    function checkIfWinning() {
+        for (let i = 0; i < winning.length; i++){
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+                return(true);
+            }
+        }
+        return(false);
+    };
 
-        switchPlayerTurn();
-        printNewRound();
+    function checkIfFull() {
+        for (let i = 0; i < board.length; i++){
+            if (board[i] == "") {
+                return (true);
+            } else {
+                continue;
+            }
+        }
+        return(false);
     }
 
-    // Initial round start
-    printNewRound();
+    function receiveInput(e) {
+        const tileInput = Number(e.target.id);
+        gameLoop(tileInput);
+    };
 
-    return {
-        playRound,
-        getActivePlayer
+    function gameLoop(tileSelection) {
+        if (makeMove(tileSelection)) {
+            if (checkIfWinning()) {
+                if (checkIfFull()){
+                    switchPlayers();
+                }
+                else {
+                    console.log("Tie!");
+                }
+            }
+            else {
+                console.log("Winner! Someone.");
+            }
+        } else {
+            console.log("Illegal Tile Placement");
+        }
+    };
+
+    
+    return {receiveInput}
+
+})();
+
+const gameBoard = (function createGameBoard () {
+    const gameBoardContainer = document.querySelector("#game-board");
+
+    for (let i = 0; i < 3; i++) {
+        const boardRow = document.createElement('div');
+        boardRow.setAttribute('class', 'game-board-row');
+        boardRow.setAttribute('id', `game-board-row-${i + 1}`);
+        for (let j = 0; j < 3; j++) {
+            const boardTile = document.createElement('div');
+            boardTile.setAttribute('class', 'board-tile');
+            boardTile.setAttribute('id', `${(i * 3) + j}`);
+            boardTile.textContent = `tile ${(i * 3) + j}`;
+            boardRow.appendChild(boardTile);
+        }
+        gameBoardContainer.appendChild(boardRow);
     }
-}
 
-const game = gameController();
+})();
+
+// gameBoard;
+
+
+
+document.querySelectorAll('.board-tile').forEach(tile => tile.addEventListener('click', gameController.receiveInput));
